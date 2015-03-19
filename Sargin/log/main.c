d55620f890a67090f071a10addac604f4bb4de61
SarginD <avterks@gmail.com>
2015-03-19 15:13:16 +0300

    little fix of path to file

--- Sargin/log/main.c
+++ Sargin/log/main.c
@@ -12,7 +12,7 @@
 
 
 int main(int argc, const char * argv[]) {
-    struct log_context logc = initlog(3, "/Users/Avterks/Desktop/log.txt", "w");
+    struct log_context logc = initlog(3, "log.txt", "w");
 
     nlog(3, &logc, "logged %d and %.1f and %d", 5, 0.5, 5);
     sleep(1);
